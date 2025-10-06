b2e8d827aa877279570736e044e799d2a52af6d6
Christoph Fink <christoph.fink@christophfink.com>
2025-10-06 20:56:16 +0200

    importlib.resources exists since 3.7

--- src/nodejs/node_path.py
+++ src/nodejs/node_path.py
@@ -4,12 +4,8 @@
 """A pathlib.Path pointing to our copy of node."""
 
 
+import importlib.resources
 import pathlib
-
-try:
-    import importlib.resources as importlib_resources
-except ImportError:
-    import importlib_resources
 
 
 __all__ = ["NODE_PATH"]
@@ -17,7 +13,7 @@ __module__ = __name__.split(".", maxsplit=1)[0]
 _NODE_PATH = "_node"
 
 
-with importlib_resources.as_file(
-    importlib_resources.files(__module__).joinpath(_NODE_PATH)
+with importlib.resources.as_file(
+    importlib.resources.files(__module__).joinpath(_NODE_PATH)
 ) as _node_path:
     NODE_PATH = pathlib.Path(_node_path).absolute()
