8425859b078eacb90a123ce9c63f3bd2d19303ec
Christoph Fink <christoph.fink@christophfink.com>
2025-10-06 16:19:31 +0200

    linted

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ import pathlib
 
 import pytest
 
-
 DATA_DIRECTORY = pathlib.Path(__file__).resolve().parent / "data"
 
 TEST_SCRIPT_ARGS = DATA_DIRECTORY / "test_args.js"
@@ -16,19 +15,19 @@ TEST_SCRIPT_HELLO = DATA_DIRECTORY / "test_hello.js"
 
 @pytest.fixture()
 def script_file_args_path():
-    """The file path to a script file that logs its own args to the console."""
+    """Return the file path to a script file that logs its own args to the console."""
     yield TEST_SCRIPT_ARGS
 
 
 @pytest.fixture()
 def script_file_hello_path():
-    """The file path to a script file that logs "hello" to the console."""
+    """Return the file path to a script file that logs "hello" to the console."""
     yield TEST_SCRIPT_HELLO
 
 
 @pytest.fixture()
 def node_path():
-    """The file path to the node installation."""
+    """Return the file path to the node installation."""
     import nodejs.node_path
 
     yield nodejs.node_path.NODE_PATH
